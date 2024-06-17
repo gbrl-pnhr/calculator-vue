03daecf38a26b18782b7dfcc98a85a408accc08b
gbrl-pnhr <143971816+gbrl-pnhr@users.noreply.github.com>
2024-06-17 14:32:29 -0300

    feat: creating style for the addition, subtraction and other operations buttons, in addition to the number buttons and the display (#1)

---
+++ src/components/Calculator.vue
@@ -0,0 +1,62 @@
+<script>
+  export default{
+    data(){
+      return{
+        keyboard1:['1', '2', '3', '+'],
+        keyboard2:['4', '5', '6', '-'],
+        keyboard3:['7', '8', '9', '*'],
+        keyboard4: [',', '0', '=', '/']
+      }
+    }
+  }
+</script>
+
+
+<template>
+  <div class = "continer">
+    <div class = "display">
+      <form action="">
+        <input type="text">
+      </form>
+    </div>
+
+    <div class="buttons">
+
+      <div class="row">
+        <div class="col-auto">
+          <button id = "reset">C</button>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-auto">
+          <button class="btn" v-for="button in keyboard1">{{ button }}</button>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-auto">
+          <button class="btn" v-for="button in keyboard2">{{ button }}</button>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-auto">
+          <button class="btn" v-for="button in keyboard3">{{ button }}</button>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-auto">
+          <button class="btn" v-for="button in keyboard4">{{ button }}</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+</template>
+
+
+<style>
+</style>
